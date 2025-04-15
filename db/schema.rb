04b9bae55abf795ec907d0fbd7919d25175c772a
violaine soulas <vhsoulas@gmail.com>
2025-04-15 11:51:53 +0200

    begin ActionCable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_14_191152) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_15_093910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_14_191152) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "nom"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "experiences", force: :cascade do |t|
     t.string "nom"
     t.text "description"
@@ -52,6 +58,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_14_191152) do
     t.float "latitude"
     t.float "longitude"
     t.string "photo_url"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "contenu"
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -86,12 +102,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_14_191152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "reservations", "experiences"
   add_foreign_key "reservations", "users"
   add_foreign_key "reviews", "experiences"
