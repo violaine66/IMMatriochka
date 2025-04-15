71eb711e1344e18c5f25fc28c21c58f0af50d14b
violaine soulas <vhsoulas@gmail.com>
2025-04-15 17:18:40 +0200

    Ajout de liens de navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :load_chatrooms
+
+  private
+
+  def load_chatrooms
+    @chatrooms = Chatroom.all
+  end
 end
