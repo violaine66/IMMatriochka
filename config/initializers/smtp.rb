282972fde408569de897438e591f0b7556ea8d40
violaine soulas <vhsoulas@gmail.com>
2025-04-28 16:43:28 +0200

    Essai de mail production

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,10 @@
-# ActionMailer::Base.smtp_settings = {
-#   address: 'smtp.postmarkapp.com',
-#   port: 587,
-#   user_name: ENV['POSTMARK_API_KEY'],
-#   password: ENV['POSTMARK_API_KEY'],
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
+# config/initializers/smtp.rb
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
