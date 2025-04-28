088713b02f755b5e3c93bb4de57be6feff76b762
violaine soulas <vhsoulas@gmail.com>
2025-04-28 08:03:57 +0200

    Changement dans application pour pb de navbar et de js pas bien chargé

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import "toastr"
+import "toastr-css"
+
+// Initialisation de Toastr
+document.addEventListener("DOMContentLoaded", () => {
+  if (flashNotice = document.querySelector('[data-flash-notice]')) {
+    toastr.success(flashNotice.dataset.flashNotice);
+  }
+  if (flashAlert = document.querySelector('[data-flash-alert]')) {
+    toastr.error(flashAlert.dataset.flashAlert);
+  }
+});
