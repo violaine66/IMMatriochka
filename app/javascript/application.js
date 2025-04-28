2050d2aa21f0bdbbfb7d330655699d6056bcc3d2
violaine soulas <vhsoulas@gmail.com>
2025-04-28 08:39:10 +0200

    Correction navbar in line

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,14 +4,3 @@ import "controllers"
 import "@popperjs/core"
 import "bootstrap"
 import "toastr"
-import "toastr-css"
-
-// Initialisation de Toastr
-document.addEventListener("DOMContentLoaded", () => {
-  if (flashNotice = document.querySelector('[data-flash-notice]')) {
-    toastr.success(flashNotice.dataset.flashNotice);
-  }
-  if (flashAlert = document.querySelector('[data-flash-alert]')) {
-    toastr.error(flashAlert.dataset.flashAlert);
-  }
-});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
-
+pin "stimulus", to: "stimulus.min.js"
+pin "turbo", to: "turbo.min.js"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
