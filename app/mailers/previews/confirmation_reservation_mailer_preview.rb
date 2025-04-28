a6d5fd8666d72b43aeb6d2fc5fae77f67bc6eca9
violaine soulas <vhsoulas@gmail.com>
2025-04-28 16:04:56 +0200

    Supprimer les prévisualisations de mails non utilisées

--- app/mailers/previews/confirmation_reservation_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-class ConfirmationReservationMailerPreview < ActionMailer::Preview
-  def creation_confirmation
-    # Ici, récupère une réservation existante ou crée une instance pour le test
-    reservation = Reservation.first
-    user = reservation.user
-    ConfirmationReservationMailer.with(reservation: reservation, user: user).creation_confirmation
-  end
-end
