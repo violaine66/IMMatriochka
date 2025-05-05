c23ed0c6ed53dfec43ad55fb479e4ef4248db54e
violaine soulas <vhsoulas@gmail.com>
2025-05-05 10:15:09 +0200

    Configuration envoi mail en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,16 @@ Rails.application.configure do
   # config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "im_matriochka_production"
 
+  # SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name => ENV["GMAIL_USERNAME"],
+  :password => ENV["GMAIL_PASSWORD"],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
